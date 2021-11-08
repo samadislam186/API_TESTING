3110e8fce4d959db9bd565e8e5de8a2337b80337
samadislam186 <48902307+samadislam186@users.noreply.github.com>
2021-11-08 15:10:06 +0530

    Second commit

---
+++ Main.py
@@ -0,0 +1,9 @@
+import requests
+
+API_STRING="http://dataservice.accuweather.com/locations/v1/adminareas/countryCode?"
+API_KEY="apikey=sihgDcEpAAc7wN2GGvBlP1ll2G0fAuVD"
+
+URI = API_STRING + API_KEY
+
+print(URI)
+
