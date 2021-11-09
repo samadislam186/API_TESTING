9675f00848e7c99ff29aad59fcf07de71009161f
AMAIISDOM\sislam <samad.islam@amadeus.com>
2021-11-09 10:09:42 +0530

    pipeline testing

--- Main.py
+++ Main.py
@@ -1,6 +1,6 @@
 import requests
 
-API_STRING="http://dataservice.accuweather.com/locations/v1/adminareas/countryCode?"
+API_STRING="http://dataservice.accuweather.com/locations/v1/adminareas/AMA?"
 API_KEY="apikey=sihgDcEpAAc7wN2GGvBlP1ll2G0fAuVD"
 
 URI = API_STRING + API_KEY
@@ -10,3 +10,10 @@ URI = API_STRING + API_KEY
 r = requests.get(URI)
 
 print(r.json())
+
+
+locationKey=r.json()
+
+print(locationKey["LocationKey"])
+
+print("Hello world")
